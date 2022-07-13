3e15afc08d4080e263025014e771a2ed6f723b08
JWoker12 <95699232+JWoker12@users.noreply.github.com>
2022-07-13 21:27:50 +0200

    finally

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -19,7 +19,35 @@ export class ItemService {
       title: 'pan',
       price: 3.8,
       quantity: 5,
-      completed: true,
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'uvas',
+      price: 13.8,
+      quantity: 10,
+      completed: false,
+    },
+    {
+      id: 4,
+      title: 'mantequilla',
+      price: 22,
+      quantity: 1,
+      completed: false,
+    },
+    {
+      id: 5,
+      title: 'spaguetti',
+      price: 5.3,
+      quantity: 3,
+      completed: false,
+    },
+    {
+      id: 6,
+      title: 'hamburguesa',
+      price: 50.8,
+      quantity: 5,
+      completed: false,
     }
   ];
 
